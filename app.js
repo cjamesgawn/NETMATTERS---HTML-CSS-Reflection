bf03a3825271e2b04e44154acf14fa3c03820d7f
cjamesgawn <69963803+cjamesgawn@users.noreply.github.com>
2021-03-01 20:10:09 +0000

    Banner-slider

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 
+const x = window.matchMedia("(min-width:992px)")
+
 $("#menu-btn").click(function(e) {
     //$(".main").css("background-color","rgba(0,0,0,0.5)");
     // console.log("menu click");
     e.preventDefault();
     e.stopPropagation();
 
-    if($('body').hasClass('menu-active')){
+    if (x.matches) {
+        
+        if($('body').hasClass('menu-active')){
+
+            $('body').removeClass('menu-active');
+            $('body').css('overflow','auto');
+            //$('.sidebar').css('transform','translateX(135%)');
+            $('.main').css('transform','translateX(0)');
+    
+        } else {
+    
+            $('body').addClass('menu-active');
+            $('body').css('overflow','hidden');
+            //$('.sidebar').css('transform','translateX(0)');
+            $('.main').css('transform','translateX(-350px)');
+    
+        }
+    } else if($('body').hasClass('menu-active')){
 
     $('body').removeClass('menu-active');
     $('body').css('overflow','auto');
@@ -17,10 +36,9 @@ $("#menu-btn").click(function(e) {
         $('body').addClass('menu-active');
         $('body').css('overflow','hidden');
         //$('.sidebar').css('transform','translateX(0)');
-        $('.main').css('transform','translateX(-350px)');
+        $('.main').css('transform','translateX(-275px)');
 
     }
-
 });
 
 $(".main").click(function() {
@@ -30,4 +48,15 @@ $(".main").click(function() {
     $('body').css('overflow','auto');
     //$('.sidebar').css('transform','translateX(135%)');
 
+});
+
+$(document).ready(function(){
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        appendDots: $('.slider-controls'),
+    });
 });
